b52699d1ee11a103d7259dcd44a3cc4eb3d96f9f
bab0n <babonoffishial@mail.ru>
2023-10-13 05:15:16 +0300

    save

--- api.py
+++ api.py
@@ -1,6 +1,7 @@
 from flask import Blueprint
 from flask_restx import fields, Api, Resource, reqparse
 from .models import User as TgUser
+from .models import db
 
 api_blueprint = Blueprint('api', __name__, url_prefix='/api/v1')
 
@@ -41,17 +42,56 @@ api.add_namespace(ns)
 class User(Resource):
     def get(self):
         parser = reqparse.RequestParser()
-        parser.add_argument('id', type=int)
+        parser.add_argument('id', type=int, required=True)
+        parser.add_argument('lol', type=int)
         args = parser.parse_args()
+        print(args)
         user = TgUser.query.get(args['id'])
-        print(user)
-        return {}, 200
+        if user is None:
+            return {'error': 'User not found'}, 400
+        t = user.__dict__
+        del t['_sa_instance_state']
+        return t, 200
 
     def post(self):
-        pass
+        parser = reqparse.RequestParser()
+        parser.add_argument('id', type=int, required=True)
+        parser.add_argument('phone', type=str, required=True)
+        parser.add_argument('name', type=str, required=True)
+        args = parser.parse_args()
+        user = TgUser.query.get(args['id'])
+        if user is not None:
+            return {'error': 'User already exist'}, 400
+        new_user = TgUser(
+            id=args['id'],
+            state='regged',
+            phone=args['phone'],
+            promo=None,
+            name=args['name'],
+            wb_api_def=None,
+            wb_api_stat=None,
+            wb_api_adv=None,
+            autofeedback=False,
+            star1=None,
+            star2=None,
+            star3=None,
+            star4=None,
+            star5=None,
+            balance=0,
+            subscribe=0,
+            upd_date=None,
+        )
+        db.session.add(new_user)
+        db.session.commit()
 
     def put(self):
-        pass
+        parser = reqparse.RequestParser()
+        parser.add_argument('id', type=int, required=True)
+        parser.add_argument('name', type=str, required=True)
+        parser.add_argument('name', type=str, required=True)
+        parser.add_argument('name', type=str, required=True)
+        args = parser.parse_args()
+        user = TgUser.query.get(args['id'])
 
     def delete(self):
         pass
