e8969665173b69799ed0cda4da4d4abb6384b017
bab0n <babonoffishial@mail.ru>
2023-12-05 02:48:22 +0300

    Include CORS for avoid some extensions

--- __init__.py
+++ __init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from flask_cors import CORS
 from . import config
 from .models import db
 
 
 def create_app():
     app = Flask(__name__)
+    CORS(app)
     app.config['SQLALCHEMY_DATABASE_URI'] = config.SQLALCHEMY_DATABASE_URI
     db.init_app(app)
 
